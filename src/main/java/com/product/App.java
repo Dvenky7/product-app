502d63c238d19e12502e6490a17b1bbe9daa05b6
Dvenky7 <venkateshdeety76@gmail.com>
2026-03-02 11:16:49 +0530

    Testing auto webhook

--- src/main/java/com/product/App.java
+++ src/main/java/com/product/App.java
@@ -5,6 +5,7 @@ package com.product;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");\
+	System.out.println("Webhook test");
     }
 }
